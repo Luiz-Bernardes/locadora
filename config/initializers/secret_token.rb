253581c295c18f0aaf3d2ebdf6ca05f7805bfd89
Luiz <luiz@luiz-Aspire-5250.(none)>
2012-10-02 10:37:58 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locadora::Application.config.secret_token = '445556dec076e0d3d6529d820ada43ad966f90627495b29005fa8d5fa7dbdd1176c789d2c52676f417882bc56e33c7a00d544e10ec953bdd07483be050f5ab73'
